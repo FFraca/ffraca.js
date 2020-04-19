e7a91ded987bb2bdeb67e242f9f7402be0e2835a
FFraca <61923573+FFraca@users.noreply.github.com>
2020-04-19 13:31:43 +0200

    Update FFraca.ja
    All functions available using help();

--- ffraca.js
+++ ffraca.js
@@ -1,3 +1,5 @@
+var nice_to_meet_you = "NICE TO MEET YOU TOO!";
+
 function random(min, max){
 	if(max === undefined){
 		min += .99999999;
@@ -25,6 +27,16 @@ String.prototype.invert = function(){
 	return output;
 }
 
+function extension(fileName){
+	let splitted = fileName.split('.');
+	if(splitted.length > 1){
+		return splitted[splitted.length - 1];
+	}
+	else{
+		return false;
+	}
+}
+
 String.prototype.replaceAll = function(search, replace) {
 	let stringa = "";
 	for(i in this){
@@ -50,6 +62,10 @@ String.prototype.replaceAll = function(search, replace) {
 		return stringa;
 	}
 };
+
+Array.prototype.random = function(){
+	return this[random(this.length - 1)];
+}
 
 function time(){
 
@@ -89,14 +105,189 @@ function time(){
 	return current;
 }
 
+function randomColor(){
+	let output = {};
+	output.RGB = {};
+	output.RGB.numbers = {};
+	output.HEX = {};
+	output.HEX.splitted = {};
+	output.RGB.numbers.r = parseInt(random(0, 255));
+	output.RGB.numbers.g = parseInt(random(0, 255));
+	output.RGB.numbers.b = parseInt(random(0, 255));
+	output.RGB.string = "rgb(" + String(output.RGB.numbers.r) + ", " + String(output.RGB.numbers.g) + ", " + String(output.RGB.numbers.b) + ")";
+	output.HEX.splitted.r = output.RGB.numbers.r.toString(16);
+	output.HEX.splitted.g = output.RGB.numbers.g.toString(16);
+	output.HEX.splitted.b = output.RGB.numbers.b.toString(16);
+	let temp = "";
+	for(var i = 0; i < 2 - output.HEX.splitted.r.length; i++){
+		temp += "0";
+	}
+	output.HEX.splitted.r = temp + output.HEX.splitted.r;
+	temp = "";
+	for(var i = 0; i < 2 - output.HEX.splitted.g.length; i++){
+		temp += "0";
+	}
+	output.HEX.splitted.g = temp + output.HEX.splitted.g;
+	temp = "";
+	for(var i = 0; i < 2 - output.HEX.splitted.b.length; i++){
+		temp += "0";
+	}
+	output.HEX.splitted.b = temp + output.HEX.splitted.b;
+	output.HEX.string = "#" + String(output.HEX.splitted.r) + String(output.HEX.splitted.g) + String(output.HEX.splitted.b);
+	return output;
+}
+
+function HTTPReq(url, output){
+	let richiesta = new XMLHttpRequest();
+	richiesta.open('GET', url);
+	richiesta.send();
+	if(output !== undefined){
+		richiesta.onreadystatechange = function(){
+			window[output] = this;
+		}
+	}
+}
+
+function age(day, month, year){
+	let current = {};
+	current.day = time().date.day.month.number;
+	current.month = time().date.month.number.number;
+	current.year = time().date.year;
+
+	let birth = {};
+	birth.day = parseInt(day);
+	birth.month = parseInt(month);
+	birth.year = parseInt(year);
+
+	let output;
+
+	let errori = "";
+
+	if(isNaN(birth.day) || (birth.day > 31 || birth.day < 1)) errori += "Day is not a valid number (1 - 31), ";
+	if(isNaN(birth.month) || (birth.month < 1 || birth.month > 12)) errori += "Month is not a valid number (1 - 12), ";
+	if(isNaN(birth.year) || (birth.year > current.year || birth.year < 1000)) errori += "Year is not a valid number (1000 - " + current.year + "), ";
+
+	if(errori.length > 0){
+		errori = errori.substring(0, errori.length - 2);
+		console.error(errori);
+		return false;
+	}
+	else{
+
+		output = current.year - birth.year;
+		if(current.month == birth.month){
+			if(current.day > birth.day){
+				output--;
+			}
+		}
+		else if(current.month < birth.month){
+			output++;
+		}
+
+		return output;
+	}
+}
+
+function hide(query, all){
+	if(all === true){
+		document.querySelectorAll(query).forEach(elemento => { elemento.classList.add('hidden') });
+	}
+	else{
+		document.querySelector(query).classList.add('hidden');	
+	}
+}
+
+function show(query, all){
+	if(all === true){
+		document.querySelectorAll(query).forEach(elemento => { elemento.classList.remove('hidden') });
+	}
+	else{
+		document.querySelector(query).classList.remove('hidden');	
+	}
+}
+
+window.addEventListener('load', () => {
+	document.querySelector('body').innerHTML += "<style>.hidden { display: none; }</style>"
+	document.querySelectorAll('button[for]').forEach(elemento => {
+		elemento.addEventListener('click', function(){
+			let tipo = document.querySelector("#" + elemento.getAttribute('for')).getAttribute('type');
+			tipo.toLowerCase() === 'password' ? document.querySelector("#" + elemento.getAttribute('for')).setAttribute('type', "text") : document.querySelector("#" + elemento.getAttribute('for')).setAttribute('type', "password");
+		});
+	});
+});
+
+function int(stringa){
+	if(typeof stringa === 'string'){
+		return parseInt(stringa.only([0,1,2,3,4,5,6,7,8,9]));
+	}
+	else{
+		console.error("In int(string), string has to be a String");
+		return false;
+	}
+}
+
+String.prototype.only = function(chars){
+	let stringa = "";
+	let output = [];
+
+	for(var i = 0; i < chars.length; i++) chars[i] = String(chars[i]);
+
+	for(i in this){
+		if(!isNaN(i)){
+			stringa += this[i];
+		}
+	}
+
+	stringa = stringa.split('');
+
+	if(typeof chars !== 'object'){
+		console.error("In String.only(chars) function, chars must be an object of type array");
+		return false
+	}
+	else{
+		let compare = 0;
+		try{ test = chars.length } catch (e) { console.error("In String.only(chars) function, chars must be an object of type array"); return false; }
+		for(var i = 0; i < stringa.length; i++){
+			if(chars.indexOf(stringa[i]) > -1){
+				output.push(stringa[i]);
+				compare++;
+			}
+		}
+		let res = "";
+		for(var i = 0; i < output.length; i++) res += output[i];
+
+		if(compare === stringa.length){
+			return true;
+		}
+		else{
+			return res;
+		}
+
+	}
+}
+
 function help(){
-	let output = `
- - help() => GET THIS
- - random(min, max) => CHOOSE A RANDOM NUMBER FROM min TO max
- - random(max) => CHOOSE A RANDOM NUMBER FROM 0 TO max
- - "your_string".replaceAll(search, replace) => REPLACE ALL search WITH replace IN "your_string"
- - "your_string".invert() => INVERT CHARS OF "your_string", OUTPUT: "gnirts_ruoy"
- - time() => GET A JSJON WITH ALL STUFF OF TIME AND DATE
+	let output = `0 help() => GET THIS
+ 1 random(min, max) => CHOOSE A RANDOM NUMBER FROM min TO max
+ 2 random(max) => CHOOSE A RANDOM NUMBER FROM 0 TO max
+ 3 "your_string".replaceAll(search, replace) => REPLACE ALL search WITH replace IN "your_string"
+ 4 "your_string".invert() => INVERT CHARS OF "your_string", OUTPUT: "gnirts_ruoy"
+ 5 time() => GET A JSON WITH ALL STUFF OF TIME AND DATE
+ 6 ["your", "array"].random() => PICK A RANDOM ITEM FROM THE ARRAY
+ 7 HTTPReq(url, "output_var") => GET FILE CONTENT OF url, INSERT IN THE STRING "output_var" THE NAME OF THE VARIABLE WHERE YOU WANT TO SAVE THE OUTPUT WHEN IT IS READY
+ 8 randomColor() => GET A RANDOM COLOR (BOTH RGB AND HEX SYNTAX)
+ 9 age(day, month, year) => GET THE AGE OF A PERSON USING HIS/HER DATE OF BIRTH
+ 10 extension(fileName) => GET THE EXTENSION FROM A FILE NAME (STRING)
+ 11 hide(query, all) => HIDE HTML OBJECT WITH THIS FUNCTION, QUERY IS THE CSS QUERY OF THE OBJECT, IF YOU WANT TO HIDE ALL OBJECTS WITH THE SAME QUERY SPECIFIED, SET THE ATTRIBUTE "all" WITH true
+ 12 show(query, all); => SHOW OBJECT/OBJECTS THAT WAS/WERE HID WITH hide FUNCTION, SAME SYNTAX
+ 13 "your_string".only(['a', 'b', 'c', 'd']) => TEST IF A STRING CONTAINS ONLY THE CHARS DECLARED IN THE PARAMETER OF THE FUNCTION USING AN ARRAY, IF SO IT RETURNS true ELSE IT RETURN THE CONVERTED STRING THAT CONTAINS ONLY THESE CHARS
+ 14 int("your_string") => FILTER ONLY NUMBERS IN A STRING, EXAMPLE: int("ASDAD998E9190") WILL RETURN 9989190
  	`;
 	return output;
 }
+
+let AS809N8D091UIJOS;
+
+setInterval(function(){
+	try{ AS809N8D091UIJOS = setInterval(loop); } catch(errore) {  }
+});
